375ba22db49e7051d84ef5030abebd72a4fdd3e2
beli10k <isabeleferreira@colegiopaulodetarso.com.br>
2025-11-12 16:03:26 +0000

    rota2

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello, Flask !!</h1>', 200
+
+
+# Exercício 2 — Versão do app
+@app.route('/versao')
+def versao():
+    versao = "1.1.0"
+    return f"App v{versao}", 200
